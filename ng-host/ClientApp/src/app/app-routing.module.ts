242bf2fd37b58dba4adcbb8fa086dc3b4bf3fc92
ttom921 <ttom921@hotmail.com>
2018-11-07 10:32:42 +0800

    加入路由(Routing)測試

--- ng-host/ClientApp/src/app/app-routing.module.ts
+++ ng-host/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page404Component } from './page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', children: [] },
+  { path: 'p1', component: Page1Component },
+  { path: 'p2', component: Page2Component },
+  { path: 'p3', component: Page3Component },
+  { path: '404', component: Page404Component },
+  { path: '**', redirectTo: '404' }
+  // { path: '**', redirectTo: ''}
+  //{ path: '**', component: Page404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-host/ClientApp/src/app/app.module.ts
+++ ng-host/ClientApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page404Component
   ],
   imports: [
     BrowserModule,
